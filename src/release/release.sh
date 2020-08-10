790fe3b54e56777323361088e6216386abf4ab3e
Dominic Zijlstra <dominiczijlstra@Dominics-Air.lan>
2020-08-10 16:09:58 +0200

    Remove stuff from release script

--- src/release/release.sh
+++ src/release/release.sh
@@ -27,25 +27,25 @@ echo Attemping to publish version: $VERSION
 [ -z "`git status --porcelain`" ] || bail "ERROR: Dirty index on working tree. Use git status to check"
 
 # Publish to pages
-rm -rf $PAGES_DIR
-git clone git@github.com:dagrejs/dagrejs.github.io.git $PAGES_DIR
+# rm -rf $PAGES_DIR
+# git clone git@github.com:dagrejs/dagrejs.github.io.git $PAGES_DIR
 
-TMP_TARGET=$PAGES_DIR/project/$PROJECT/latest
-rm -rf $TMP_TARGET
-mkdir -p $TMP_TARGET
-cp -r $DIST_DIR/*.js $TMP_TARGET
+# TMP_TARGET=$PAGES_DIR/project/$PROJECT/latest
+# rm -rf $TMP_TARGET
+# mkdir -p $TMP_TARGET
+# cp -r $DIST_DIR/*.js $TMP_TARGET
 
-TMP_TARGET=$PAGES_DIR/project/$PROJECT/v$VERSION
-rm -rf $TMP_TARGET
-mkdir -p $TMP_TARGET
-cp -r $DIST_DIR/*.js $TMP_TARGET
+# TMP_TARGET=$PAGES_DIR/project/$PROJECT/v$VERSION
+# rm -rf $TMP_TARGET
+# mkdir -p $TMP_TARGET
+# cp -r $DIST_DIR/*.js $TMP_TARGET
 
-cd $PAGES_DIR/project/$PROJECT
-git add -A
-git commit -m "Publishing $PROJECT v$VERSION"
-git push -f origin master
-cd $PROJECT_ROOT
-echo "Published $PROJECT to pages"
+# cd $PAGES_DIR/project/$PROJECT
+# git add -A
+# git commit -m "Publishing $PROJECT v$VERSION"
+# git push -f origin master
+# cd $PROJECT_ROOT
+# echo "Published $PROJECT to pages"
 
 # Publish tag
 git tag v$VERSION
